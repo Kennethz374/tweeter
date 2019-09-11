9a94047b4a6fd8f1a90404f432943640e46ae6a0
vagrant <Kennethzhang374@gmail.com>
2019-09-11 19:49:18 +0000

    added timeHelper script to help on displaying better time logo

---
+++ public/scripts/timeHelper.js
@@ -0,0 +1,25 @@
+
+const timeHelper = function (ms) {
+  if(ms >= 12 * 30 * 24 * 3600000) {
+    return `${Math.floor(ms/(12 * 30 * 24 * 3600000))} years ago`
+  } else if (ms >= 30 * 24 * 3600000) {
+    return `${Math.floor(ms/(30 * 24 * 3600000))} months ago`
+  } else if (ms >=  (7 * 24 * 3600000)) {
+    return `${Math.floor(ms/(7 * 24 * 3600000))} weeks ago`
+  } else if (ms >= 24 * 3600000) {
+    return `${Math.floor(ms/(24 * 3600000))} days ago`
+  } else if (ms >= 3600000) {
+    return `${Math.floor(ms/3600000)} hours ago`
+  } else if (ms >= 60000) {
+    return `${Math.floor(ms/60000)} hours ago`
+  }
+}
+
+
+//1 minute = 60000ms
+//1 hour   = 360 0000ms
+//1 day    = 24 * 360 0000ms
+//1 week   = 7 * 24 * 360 0000ms
+//1 month  = 30  * 24 * 360 0000ms
+//1 year   = 12 * 30 * 24 * 360 0000ms
+//1568218105806-1461113959088 = 107104146718 ms
