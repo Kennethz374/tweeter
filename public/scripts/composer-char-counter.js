cfaa5d135bb7a21724f39bacdac9c56a89e8c0ac
vagrant <Kennethzhang374@gmail.com>
2019-09-11 19:45:37 +0000

    added srcipt for counter for form, input

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+  // $("#tweetMsg").on("keyup", function(){
+  //   console.log("123123123");
+  // })
+
+  // $("#tweetMsg").on("keypress", function(){
+  //   console.log("123123123");
+  // });
+
+  $("textarea").on("keyup", function(){
+    let inputLength = $(this).val().length;
+    const counter = $(this).siblings(".counter");//save the counter element into variable counter;
+    counter.text(`${140 - inputLength}`);
+    if(inputLength > 140) {
+      counter.addClass("counterNeg");
+    } else {
+      counter.removeClass("counterNeg");
+    }
+  })
+
+  // $(".counter").text(``)
+
+  // $("#tweetMsg").on("change", function(){
+  //   console.log("123123123");
+  // })
+});
